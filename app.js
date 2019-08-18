bb88b75aaeaac9a4d689d64ff41cdab97bfc2016
Dmitriy Palchuk <dmitry.palchuk@vrpconsulting.com>
2019-08-18 18:03:07 +0300

    fixed auth callback URI

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ let jsforce      = require('jsforce');
 const CONSUMER_KEY    = '3MVG9G9pzCUSkzZshQPF13Ib7bBPk2kQ3pVcnAyPePRUgLyC4eMsa4CasJMeNNBUuOKWHNG2zmSKUKt7bBFth';
 const CONSUMER_SECRET = '0A76794BE73E909EDB0A93C24CC83C38791E246234EA1E5D0484D8360F279B54';
 const ENDPOINT        = 'https://lwc-with-lightning-out-dev-ed.lightning.force.com';
-const REDIRECT_URI    = 'https://lwc-with-lightning-out.herokuapp.com//oauth2/callback';
+const REDIRECT_URI    = 'https://lwc-with-lightning-out.herokuapp.com/oauth2/callback';
 
 let token = '';
 
