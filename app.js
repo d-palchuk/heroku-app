b974c46cec3c8969568f70642792ded9716a102f
Dmitriy Palchuk <dmitry.palchuk@vrpconsulting.com>
2019-08-19 20:09:20 +0300

    removed CORS

--- app.js
+++ app.js
@@ -26,18 +26,6 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*Allow CORS*/
-app.use(function(req, res, next) {
-
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization,X-Authorization'); 
-	res.setHeader('Access-Control-Allow-Methods', '*');
-	res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
-	res.setHeader('Access-Control-Max-Age', '1000');
-
-	next();
-});
-
 //
 // OAuth2 client information can be shared with multiple connections.
 //
