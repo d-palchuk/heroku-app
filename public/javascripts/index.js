b2fe1bc734c1887aade466ee9b9cdab312d1dc6c
Dmitriy Palchuk <dmitry.palchuk@vrpconsulting.com>
2019-08-21 22:45:47 +0300

    added form component to sidebar

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,22 +3,37 @@ fetch('https://lwc-with-lightning-out.herokuapp.com/token')
     return response.json();
   })
   .then(data => {
-    $Lightning.use("c:lightningOutApp", () => {
-      $Lightning.createComponent(
-        "c:lightningOutCmp",
-        {},
-        "lightningOutContainer",
-        (cmp) => {
-          console.log("LWC component was created");
-          // do some stuff
-        }
-      );
-    },
-    'https://lwc-with-lightning-out-dev-ed.lightning.force.com',
-    data.token
-    );
+    createLightningComponents(data.token);
   })
   .catch(error => {
     console.log('NO TOKEN')
   });
 
+
+function createLightningComponents(token) {
+  $Lightning.use("c:lightningOutApp", () => {
+
+    $Lightning.createComponent(
+      "c:lightningOutCmp",
+      {},
+      "main",
+      (cmp) => {
+        console.log("LWC component was created");
+        // do some stuff
+      }
+    );
+
+    $Lightning.createComponent(
+      "c:formCreateReadEditAccount",
+      {},
+      "sidebar",
+      (cmp) => {
+        console.log("LWC component was created");
+        // do some stuff
+      }
+    );
+  },
+  'https://lwc-with-lightning-out-dev-ed.lightning.force.com',
+  token
+  );
+}
