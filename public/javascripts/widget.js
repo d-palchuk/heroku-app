847169c46ef054e5fba555c7ec61c742c6dd966f
Dmitriy Palchuk <dmitry.palchuk@vrpconsulting.com>
2019-08-26 09:36:15 +0300

    removed IIFE

--- public/javascripts/widget.js
+++ public/javascripts/widget.js
@@ -1,17 +1,16 @@
-(function createLightningComponents(token) {
-    $Lightning.use("c:lightningOutApp", () => {
-      $Lightning.createComponent(
-        "c:widgetFoodDelivery",
-        {
-            restaurantId : '0012v00002STk9AAAT'
-        },
-        "main",
-        (cmp) => {
-          console.log("LWC component was created");
-          // do some stuff
-        }
-      );
+
+$Lightning.use("c:lightningOutApp", () => {
+    $Lightning.createComponent(
+    "c:widgetFoodDelivery",
+    {
+        restaurantId : '0012v00002STk9AAAT'
     },
-    'https://food-delivery-developer-edition.ap15.force.com/customers/'
+    "main",
+    (cmp) => {
+        console.log("LWC component was created");
+        // do some stuff
+    }
     );
-  })()
+},
+'https://food-delivery-developer-edition.ap15.force.com/customers/'
+);
