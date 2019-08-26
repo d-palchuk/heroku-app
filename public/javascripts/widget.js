0abe8f2a5327a05ba2772b9c8b2f715e8d5c4c53
Dmitriy Palchuk <dmitry.palchuk@vrpconsulting.com>
2019-08-26 09:33:05 +0300

    returned IIFE

--- public/javascripts/widget.js
+++ public/javascripts/widget.js
@@ -1,16 +1,17 @@
-
-$Lightning.use("c:lightningOutApp", () => {
-    $Lightning.createComponent(
-    "c:widgetFoodDelivery",
-    {
-        restaurantId : '0012v00002STk9AAAT'
+(function createLightningComponents(token) {
+    $Lightning.use("c:lightningOutApp", () => {
+      $Lightning.createComponent(
+        "c:widgetFoodDelivery",
+        {
+            restaurantId : '0012v00002STk9AAAT'
+        },
+        "main",
+        (cmp) => {
+          console.log("LWC component was created");
+          // do some stuff
+        }
+      );
     },
-    "main",
-    (cmp) => {
-        console.log("LWC component was created");
-        // do some stuff
-    }
+    'https://food-delivery-developer-edition.ap15.force.com/customers/'
     );
-},
-'https://food-delivery-developer-edition.ap15.force.com/customers/'
-);
+  })()
